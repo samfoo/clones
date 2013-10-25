71e2051d93ac615cb83d34ffa1be2721ec54db69
Sam Gibson <sam@ifdown.net>
2013-10-25 11:31:47 +0200

    Add ines parsing

---
+++ spec/clones/nes/rom_spec.clj
@@ -0,0 +1,90 @@
+(ns clones.nes.rom-spec
+  (:require [speclj.core    :refer :all]
+            [clones.nes.rom :refer :all]))
+
+(def invalid-header (repeat 16 0))
+(def valid-header [78 69 83 26 1 1 0 0 0 0 0 0 0 0 0 0])
+
+(def with-mapper-1 [78 69 83 26 1 1 16 0 0 0 0 0 0 0 0 0])
+(def with-mapper-16 [78 69 83 26 1 1 0 16 0 0 0 0 0 0 0 0])
+(def with-mapper-17 [78 69 83 26 1 1 16 16 0 0 0 0 0 0 0 0])
+
+(def with-four-screen-mirroring [78 69 83 26 1 1 8 0 0 0 0 0 0 0 0 0])
+(def with-horizontal-mirroring [78 69 83 26 1 1 0 0 0 0 0 0 0 0 0 0])
+(def with-vertical-mirroring [78 69 83 26 1 1 1 0 0 0 0 0 0 0 0 0])
+
+(def with-trainer [78 69 83 26 1 1 4 0 0 0 0 0 0 0 0 0 0])
+(def with-battery [78 69 83 26 1 1 2 0 0 0 0 0 0 0 0 0 0])
+
+(def with-playchoice [78 69 83 26 1 1 0 2 0 0 0 0 0 0 0 0 0])
+(def with-vs-unisystem [78 69 83 26 1 1 0 1 0 0 0 0 0 0 0 0 0])
+
+(def with-nes-2 [78 69 83 26 1 1 0 8 0 0 0 0 0 0 0 0 0])
+
+(describe "An iNES rom parser"
+  (describe "parse-ines-header"
+    (it "should read that it is not NES-2 format if bits 3 & 2 of the 7th byte aren't 10"
+      (should-not (:nes-2? (parse-ines-header valid-header))))
+
+    (it "should read that it is NES-2 format if bits 3 & 2 of the 7th byte are 10"
+      (should (:nes-2? (parse-ines-header with-nes-2))))
+
+    (it "should read that it is not a vs-unisystem if bit 0 of the 7th byte is 0"
+      (should-not (:vs-unisystem? (parse-ines-header valid-header))))
+
+    (it "should read that it is a vs-unisystem if bit 0 of the 7th byte is 1"
+      (should (:vs-unisystem? (parse-ines-header with-vs-unisystem))))
+
+    (it "should read that it is not a playchoice-10 if bit 1 of the 7th byte is 0"
+      (should-not (:playchoice-10? (parse-ines-header valid-header))))
+
+    (it "should read that it is a playchoice-10 if bit 1 of the 7th byte is 1"
+      (should (:playchoice-10? (parse-ines-header with-playchoice))))
+
+    (it "should read that there is no battery backing if bit 1 of the 6th byte is 0"
+      (should-not (:battery-backed? (parse-ines-header valid-header))))
+
+    (it "should read that there is a battery backing if bit 1 of the 6th byte is 1"
+      (should (:battery-backed? (parse-ines-header with-battery))))
+
+    (it "should read that there is no trainer if bit 2 of the 6th byte is 0"
+      (should-not (:trainer-present? (parse-ines-header valid-header))))
+
+    (it "should read that there is a trainer if bit 2 of the 6th byte is 1"
+      (should (:trainer-present? (parse-ines-header with-trainer))))
+
+    (it "should read the mirroring mode as the combination of the third and
+        0th bit in the 6th byte
+
+        0xx0 -> horizontal mirroring
+        0xx1 -> vertical mirroring
+        1xxx -> four-way mirroring"
+      (should= :vertical (:mirroring (parse-ines-header with-vertical-mirroring)))
+      (should= :horizontal (:mirroring (parse-ines-header with-horizontal-mirroring)))
+      (should= :four-screen (:mirroring (parse-ines-header with-four-screen-mirroring))))
+
+    (it "should or both the lower and upper nibbles for the mapper together"
+      (should= 17
+               (:mapper (parse-ines-header with-mapper-17))))
+
+    (it "should read the mapper number upper nibble as the first four bits of
+        the 7th byte"
+      (should= 16
+               (:mapper (parse-ines-header with-mapper-16))))
+
+    (it "should read the mapper number lower nibble as the first four bits of
+         the 6th byte"
+      (should= 1
+               (:mapper (parse-ines-header with-mapper-1))))
+
+    (it "should read the 6th byte as the pattern table size in 8KB units converted to number of bytes"
+      (should= 8192
+               (:chr-rom-size (parse-ines-header valid-header))))
+
+    (it "should read the 5th byte as the program rom size in 16KB units converted to number of bytes"
+      (should= 16384
+               (:prg-rom-size (parse-ines-header valid-header))))
+
+    (it "should throw an error if the first four bytes don't match the expected signature"
+      (should-throw Error "Invalid iNES file"
+        (parse-ines-header invalid-header)))))
---
+++ src/clones/nes/rom.clj
@@ -0,0 +1,158 @@
+(ns clones.nes.rom
+  (:use [clones.byte :refer :all]
+        [clojure.java.io :only [file input-stream]]))
+
+(def mapper-names {
+    0   "NROM",
+    1   "SxROM, MMC1",
+    2   "UxROM",
+    3   "CNROM",
+    4   "TxROM, MMC3, MMC6",
+    5   "ExROM, MMC5",
+    7   "AxROM",
+    9   "PxROM, MMC2",
+    10  "FxROM, MMC4",
+    11  "Color Dreams",
+    13  "CPROM",
+    15  "100-in-1 Contra Function 16",
+    16  "Bandai EPROM (24C02)",
+    18  "Jaleco SS8806",
+    19  "Namco 163",
+    21  "VRC4a, VRC4c",
+    22  "VRC2a",
+    23  "VRC2b, VRC4e",
+    24  "VRC6a",
+    25  "VRC4b, VRC4d",
+    26  "VRC6b",
+    34  "BNROM, NINA-001",
+    64  "RAMBO-1",
+    66  "GxROM, MxROM",
+    68  "After Burner",
+    69  "FME-7, Sunsoft 5B",
+    71  "Camerica/Codemasters",
+    73  "VRC3",
+    74  "Pirate MMC3 derivative",
+    75  "VRC1",
+    76  "Namco 109 variant",
+    79  "NINA-03/NINA-06",
+    85  "VRC7",
+    86  "JALECO-JF-13",
+    94  "Senjou no Ookami",
+    105 "NES-EVENT Similar to MMC1",
+    113 "NINA-03/NINA-06??",
+    118 "TxSROM, MMC3",
+    119 "TQROM, MMC3",
+    159 "Bandai EPROM (24C01)",
+    166 "SUBOR",
+    167 "SUBOR",
+    180 "Crazy Climber",
+    185 "CNROM with protection diodes",
+    192 "Pirate MMC3 derivative",
+    206 "DxROM, Namco 118 / MIMIC-1",
+    210 "Namco 175 and 340",
+    228 "Action 52",
+    232 "Camerica/Codemasters Quattro",
+})
+
+(defn- bit-mask [n]
+  (if (> n 0)
+    (bit-or
+      (bit-shift-left 1 (dec n))
+      (bit-mask (dec n)))
+    0))
+
+(defn- sum [s] (reduce #(+ %1 %2) 0 s))
+
+(defn- bit-val [b len offset]
+  (let [shift-amt (- offset len)
+        mask (bit-shift-left
+               (bit-mask len)
+               shift-amt)
+        result (bit-shift-right
+                 (bit-and b mask)
+                 shift-amt)]
+    (if (= 1 len)
+      (= result 1)
+      result)))
+
+(defn- bit-seq [b bit-lengths]
+  (map-indexed
+    (fn [i len]
+      (let [offset (- 8 (sum (take i bit-lengths)))]
+        (bit-val b len offset)))
+    bit-lengths))
+
+(defn- bit-map [b & desc]
+  (let [ks (map first (partition 2 desc))
+        vs (map second (partition 2 desc))]
+    (zipmap ks (bit-seq b vs))))
+
+(defn parse-ines-header [data]
+  (let [sig (take 4 data)
+        prg-rom-size (* 16384 (nth data 4))
+        chr-rom-size (* 8192 (nth data 5))
+        flags-6 (bit-map (nth data 6)
+                         :lower-mapper-nibble 4
+                         :mirroring-upper 1
+                         :trainer-present? 1
+                         :battery-backed? 1
+                         :mirroring-lower 1)
+        flags-7 (bit-map (nth data 7)
+                         :upper-mapper-nibble 4
+                         :nes-2 2
+                         :playchoice-10? 1
+                         :vs-unisystem? 1)
+        mapper (bit-or (:lower-mapper-nibble flags-6)
+                       (bit-shift-left
+                         (:upper-mapper-nibble flags-7)
+                         4))
+        mirroring (if (:mirroring-upper flags-6)
+                    :four-screen
+                    (if (:mirroring-lower flags-6)
+                      :vertical
+                      :horizontal))
+        nes-2? (= 2 (:nes-2 flags-7))]
+    (if (not= sig [78 69 83 26])
+      (throw (Error. "Invalid iNES file"))
+      (merge {:prg-rom-size prg-rom-size
+              :chr-rom-size chr-rom-size
+              :mapper mapper
+              :mapper-name (get mapper-names mapper)
+              :mirroring mirroring
+              :nes-2? nes-2?}
+
+             (select-keys flags-6 [:trainer-present?
+                                   :battery-backed?])
+
+             (select-keys flags-7 [:playchoice-10?
+                                   :vs-unisystem?])))))
+
+(defn file-data [filename]
+  (with-open [f (input-stream filename)]
+    (let [size (.length (file filename))
+          ba (byte-array size)]
+      (.read f ba)
+      (map unsigned-byte (sequence ba)))))
+
+(def header-size 16)
+(def trainer-size 512)
+
+(defn read-rom [filename]
+  (let [data (file-data filename)
+        header-data (take header-size data)
+        header (parse-ines-header header-data)
+        prg-offset (if (:trainer-present? header)
+                     (+ header-size trainer-size)
+                     header-size)
+        prg-data (->> data
+                   (drop prg-offset)
+                   (take (:prg-rom-size header)))
+        chr-offset (if (:trainer-present? header)
+                     (+ header-size trainer-size (:prg-rom-size header))
+                     (+ header-size (:prg-rom-size header)))
+        chr-data (->> data
+                   (drop chr-offset)
+                   (take (:chr-rom-size header)))]
+    (merge header {:prg-data prg-data
+                   :chr-data chr-data})))
+
